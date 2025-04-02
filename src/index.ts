ee1bf7247d0406b1c769a82ad8ba70cd035bb87f
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-04-02 19:57:52 +0300

    style: add foundational SCSS structure with global and utility files
    - Added variables.scss for defining global SCSS variables.
    - Created global.scss for general styles and layout configurations.
    - Implemented main.scss as the entry point for SCSS compilation.
    - Added reset.scss to normalize styles across browsers.
    - Introduced mixins.scss for reusable SCSS mixins.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import Phaser from 'phaser';
 import { StartScene } from './scenes/StartScene';
 import { GameScene } from './scenes/GameScene';
 
+import './styles/main.scss';
+
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 800,

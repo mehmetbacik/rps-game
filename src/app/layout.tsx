2c5bd84b1e1b8f0aea09b1f2ad0af9426bb0812b
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-04-08 15:30:17 +0300

    style: add foundational SCSS structure with global and utility files
    - Added variables.scss for defining global SCSS variables.
    - Created global.scss for general styles and layout configurations.
    - Implemented main.scss as the entry point for SCSS compilation.
    - Added reset.scss to normalize styles across browsers.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { GameProvider } from '../context/GameContext';
-import "@/styles/globals.scss";
+import "@/styles/main.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

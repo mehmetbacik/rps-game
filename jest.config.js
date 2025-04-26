0692dc7b99a458c7e5bd67f279e82f28ceaa9a4f
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2025-04-26 22:50:56 +0300

    fix: resolve issue for jest
    - Removed and reinstalled dependencies to ensure @testing-library/jest-dom is correctly installed and accessible.
    - Verified jest.setup.ts file and ensured it is correctly configured in the Jest setup.
    - Cleaned up node_modules and reinstalled all packages to address potential issues with missing or incorrect package versions.
    - Updated Jest configuration to ensure proper setup of test environment.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'jsdom',
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
+  moduleNameMapper: {
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/public/(.*)$': '<rootDir>/public/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
